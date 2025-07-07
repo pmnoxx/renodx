1b82570a437037840147ab6e70777e066f2e3a30
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-07-07 10:32:54 -0700

    fix ubers

--- src/games/_hbr/uber_park_0xFBCF41E6.ps_4_0.hlsl
+++ src/games/_hbr/uber_park_0xFBCF41E6.ps_4_0.hlsl
@@ -97,7 +97,7 @@ void main(
 
     if (RENODX_TONE_MAP_TYPE != 0) {
       float3 sdrGraded = r0.xyz;
-      o0.rgb = renodx::draw::ToneMapPass(untonemapped, sdrGraded); // all 3 colors are in LINEAR here
+      r0.rgb = renodx::draw::ToneMapPass(untonemapped, sdrGraded); // all 3 colors are in LINEAR here
     }
 
     
@@ -108,7 +108,7 @@ void main(
     } else {
       o0.w = r2.w;
     }
-    o0.xyz = renodx::draw::RenderIntermediatePass(r0.xyz);
+    r0.xyz = renodx::draw::RenderIntermediatePass(r0.xyz);
     return;
   }
 
