d7ab8077569adfaea493326da24f5f4205b57049
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-07-11 09:38:17 -0700

    add darkest dungeons

--- src/games/_darkestdungeons2/swap_chain_proxy_pixel_shader.ps_5_x.hlsl
+++ src/games/_darkestdungeons2/swap_chain_proxy_pixel_shader.ps_5_x.hlsl
@@ -1,8 +1,15 @@
-#include "./shared.h"
+#include "./common.h"
 
 Texture2D t0 : register(t0);
 SamplerState s0 : register(s0);
 float4 main(float4 vpos: SV_POSITION, float2 uv: TEXCOORD0)
     : SV_TARGET {
-  return renodx::draw::SwapChainPass(t0.Sample(s0, uv));
+
+
+  float4 color = t0.Sample(s0, uv);
+ // color.xyz = renodx::draw::ToneMapPass(color.xyz);
+  color.xyz = ApplyPerceptualBoostAndToneMap(color.xyz, SCENE_TYPE_2D_BACKGROND, uv);
+  color.xyz = renodx::draw::RenderIntermediatePass(color.xyz);
+
+  return renodx::draw::SwapChainPass(color);
 }
