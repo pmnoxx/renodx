096bb7eacbdf6a4c71f3230ed68a08001b69bc31
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-07-11 09:38:01 -0700

    Fix glitches with tonemappass in HBR

--- src/games/_hbr/shared.h
+++ src/games/_hbr/shared.h
@@ -75,7 +75,7 @@ struct ShaderInjectData {
   float perceptual_boost_param;
   float perceptual_boost_color;
   float perceptual_boost_strength;
-  float pixel_shader_decode_mode; // 0 = Off, 1 = Gamma 2.2, 2 = sRGB
+//  float pixel_shader_decode_mode; // 0 = Off, 1 = Gamma 2.2, 2 = sRGB
 
   float effect_split_x ;
   float effect_split_mode ;
