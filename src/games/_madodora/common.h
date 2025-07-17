3b835030b30b3d91ad3d2e1f85829f1f307c7d97
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-07-16 23:59:49 -0700

    Change ordering of applying tonemapping

--- src/games/_madodora/common.h
+++ src/games/_madodora/common.h
@@ -1,8 +1,7 @@
-#include "./shared.h"
-
 #ifndef COMMON_H_
 #define COMMON_H_
 
+#include "./shared.h"
 
 #define INV_REINHARD 2.f
 
@@ -26,13 +25,21 @@ float ReinhardScalable(float color, float channel_max = 1.f, float channel_min =
 #define SCENE_TYPE_3D 3
 
 
-float4 debug_mode(float4 color, float2 pos) {
-    if (1.f - pos.x < 0.01f * RENODX_DEBUG_MODE / 3.f && 1.f - pos.y < 0.01f * RENODX_DEBUG_MODE && RENODX_DEBUG_MODE)
-        return float4(1.f, 0.f, 0.f, 1.f);
-    if (1.f - pos.x < 0.01f * RENODX_DEBUG_MODE * 2.f / 3.f && 1.f - pos.y < 0.01f * RENODX_DEBUG_MODE && RENODX_DEBUG_MODE)
-        return float4(0.f, 1.f, 0.f, 1.f);
-    if (1.f - pos.x < 0.01f * RENODX_DEBUG_MODE * 3.f / 3.f && 1.f - pos.y < 0.01f * RENODX_DEBUG_MODE && RENODX_DEBUG_MODE)
+float4 debug_mode(float4 color, float2 pos, float shift_y = 0.f) {
+    float2 box = float2(0.99f, 0.99f - shift_y);
+    float2 dim = float2(0.01f, 0.01f);
+
+    if (length(pos >= box && pos <= box + dim) > 1.f) {
+        float part = (pos.x - box.x) / dim.x;
+        if (part < 1. / 3.f) {
+            return float4(1.f, 0.f, 0.f, 1.f);
+        }
+        if (part < 2. / 3.f) {
+            return float4(0.f, 1.f, 0.f, 1.f);
+        }     
         return float4(0.f, 0.f, 1.f, 1.f);
+    }
+
     return color;
 }
   
@@ -61,12 +68,10 @@ float3 ToneMapPassWrapper(float3 untonemapped, float3 graded_sdr_color, float3 n
 
 float4 renodx_opening_tonemap_block(float4 r0, float2 v1, Texture2D<float4> t2, Texture2D<float4> t3, float use_t3, bool useToneMapPass) {
 
-    if (useToneMapPass) {
-        r0.rgb = renodx::draw::ToneMapPass(r0.rgb);
-    }
     float4 r1 = 0;
     r1.w = use_t3;
     if (r1.w != 0) {
+      r0 = debug_mode(r0, v1, 0.02f);
       float3 untonemapped = r0.xyz;
       float3 sdrTonemapped = renodx::tonemap::renodrt::NeutralSDR(untonemapped);  // tonemap to SDR you can change this to any SDR tonemapper you want
       if (RENODX_TONE_MAP_TYPE != 0) {
@@ -86,6 +91,9 @@ float4 renodx_opening_tonemap_block(float4 r0, float2 v1, Texture2D<float4> t2, 
     r0.xyz = renodx::color::pq::Encode(r0.xyz, 100.f);
     r0.xyz = renodx::lut::SampleTetrahedral(t2, r0.xyz);
 
+    if (useToneMapPass) {
+        r0.rgb = renodx::draw::ToneMapPass(r0.rgb);
+    }
     r0.w = 1;
     r0.xyz = renodx::draw::RenderIntermediatePass(r0.xyz);
     return r0;
