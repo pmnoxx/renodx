b44f7648786b9003b2921c6481adb142d78382bc
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-07-17 00:47:48 -0700

    Fix login screen

--- src/games/_madodora/uber_ult2_0x0DE9EBCF.ps_4_0.hlsl
+++ src/games/_madodora/uber_ult2_0x0DE9EBCF.ps_4_0.hlsl
@@ -48,15 +48,15 @@ void main(
   }
   r0.xyz = cb0[132].www * r0.xyz;
 
-
-  r0 = debug_mode(r0,v1);
-
+  r0 = debug_mode(r0, v1);
+  float4 untonemapped = r0;
+/*
   float3 preCG = r0.xyz;
 
   if (RENODX_TONE_MAP_TYPE != 0) {
     o0 = renodx_opening_tonemap_block(r0, v1, t1, t2, cmp(0 < cb0[133].w), true);
     return;
-  }
+  }*/
 
   r1.y = dot(float3(0.439700991,0.382977992,0.177334994), r0.xyz);
   r1.z = dot(float3(0.0897922963,0.813422978,0.0967615992), r0.xyz);
@@ -189,9 +189,9 @@ void main(
   r1.y = (dot(float3(-0.969243646,1.8759675,0.0415550582), r0.xyz));
   r1.z = (dot(float3(0.0556300804,-0.203976959,1.05697155), r0.xyz));
 
-  if (RENODX_TONE_MAP_TYPE == 0.f) {
-    r1.xyz = saturate(r1.xyz);
-  }
+//  if (RENODX_TONE_MAP_TYPE == 0.f) {
+  //  r1.xyz = saturate(r1.xyz);
+ // }
 
   r0.x = cmp(0 < cb0[133].w);
   if (r0.x != 0) {
@@ -225,6 +225,10 @@ void main(
     r3.xyz = exp2(r3.xyz);
     r0.xyz = cmp(float3(0.0404499993,0.0404499993,0.0404499993) >= r0.xyz);
     r1.xyz = r0.xyz ? r2.xyz : r3.xyz;
+  }
+  if (RENODX_TONE_MAP_TYPE != 0.f) {
+    o0 = renodx_opening_tonemap_block(untonemapped, saturate(r1), v1, t1, t2, cmp(0 < 0), true);
+    return;
   }
   r0.xyz = cb0[132].zzz * r1.zxy;
   r0.x = floor(r0.x);
--- src/games/_madodora/uber_ult_0xE5C37261.ps_4_0.hlsl
+++ src/games/_madodora/uber_ult_0xE5C37261.ps_4_0.hlsl
@@ -67,13 +67,14 @@ void main(
   }
   r0.xyz = cb0[132].www * r0.xyz;
 
-  r0 = debug_mode(r0,v1);
-
+  r0 = debug_mode(r0, v1);
+  float4 untonemapped = r0;
+/*
   if (RENODX_TONE_MAP_TYPE != 0) {
     o0 = renodx_opening_tonemap_block(r0, v1, t2, t3, cmp(0 < cb0[133].w), true);
     return;
   }
-
+*/
   r1.y = dot(float3(0.439700991,0.382977992,0.177334994), r0.xyz);
   r1.z = dot(float3(0.0897922963,0.813422978,0.0967615992), r0.xyz);
   r1.w = dot(float3(0.0175439995,0.111543998,0.870703995), r0.xyz);
@@ -205,9 +206,9 @@ void main(
   r1.y = (dot(float3(-0.969243646,1.8759675,0.0415550582), r0.xyz));
   r1.z = (dot(float3(0.0556300804, -0.203976959, 1.05697155), r0.xyz));
 
-  if (RENODX_TONE_MAP_TYPE == 0.f) {
+ // if (RENODX_TONE_MAP_TYPE == 0.f) {
     r1.xyz = saturate(r1.xyz);
-  }
+//  }
 
   r0.x = cmp(0 < cb0[133].w);
   if (r0.x != 0) {
@@ -242,6 +243,11 @@ void main(
     r0.xyz = cmp(float3(0.0404499993,0.0404499993,0.0404499993) >= r0.xyz);
     r1.xyz = r0.xyz ? r2.xyz : r3.xyz;
   }
+  if (RENODX_TONE_MAP_TYPE != 0.f) {
+    o0 = renodx_opening_tonemap_block(untonemapped, saturate(r1), v1, t2, t3, cmp(0 < 0), true);
+    return;
+  }
+
   r0.xyz = cb0[132].zzz * r1.zxy;
   r0.x = floor(r0.x);
   r1.xy = float2(0.5,0.5) * cb0[132].xy;
