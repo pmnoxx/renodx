5099f33b53cfa3e75cd6750d4d99f21bf474c1d7
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-07-21 14:04:46 -0700

    Add support for Against The Storm

--- src/games/_againstthestorm/lutbuilders/lut_builder_ldr_0xED457D04.ps_4_0.hlsl
+++ src/games/_againstthestorm/lutbuilders/lut_builder_ldr_0xED457D04.ps_4_0.hlsl
@@ -1,3 +1,5 @@
+#include "../custom.hlsl"
+
 // ---- Created with 3Dmigoto v1.4.1 on Thu Jul 10 16:54:45 2025
 Texture2D<float4> t7 : register(t7);
 
@@ -44,6 +46,11 @@ void main(
   r1.x = r0.x / cb0[128].x;
   r0.w = -r1.x + r0.y;
   r0.xyz = cb0[128].www * r0.xzw;
+
+  if (RENODX_TONE_MAP_TYPE > 0.f) {
+    r0.xyz = renodx::color::pq::Decode(r0.xyz, 1.f);  // Decode from PQ to linear
+    r0.xyz = renodx::color::bt709::from::BT2020(r0.xyz);
+  }
 
   // linear to ...
   r1.x = dot(float3(0.390404999,0.549941003,0.00892631989), r0.xyz);
@@ -200,6 +207,16 @@ void main(
   r0.x = r0.x * r0.y;
   r0.x = cb0[134].y * r0.x;
   r0.xyz = r0.xxx * r1.xyz + r0.zzz;
+
+  float3 untonemapped = r0.xyz;
+
+  float3 untonemappedSign = sign(untonemapped);
+  untonemapped = abs(untonemapped);
+  float3 sdrTonemapped = saturate(untonemapped = abs(untonemapped));
+  r0.xyz = sdrTonemapped;
+
+  r0.xyz = abs(sdrTonemapped);
+
   r0.xyz = float3(0.00390625,0.00390625,0.00390625) + r0.xyz;
   r0.w = 0;
   r1.xyzw = t0.SampleBias(s0_s, r0.xw, cb0[19].x).xyzw;
@@ -217,5 +234,14 @@ void main(
   o0.z = saturate(r0.x);
   o0.y = saturate(r1.x);
   o0.w = 1;
+
+  if (RENODX_TONE_MAP_TYPE != 0) {
+    float3 sdrGraded = o0.xyz;
+    o0.r = UpgradeToneMapCustom(untonemapped.r, sdrTonemapped.r, sdrGraded.r, 1.f).r;
+    o0.g = UpgradeToneMapCustom(untonemapped.g, sdrTonemapped.g, sdrGraded.g, 1.f).g;
+    o0.b = UpgradeToneMapCustom(untonemapped.b, sdrTonemapped.b, sdrGraded.b, 1.f).b;
+    // o0.rgb = ToneMapPassWrapper(color);  // all 3 colors are in LINEAR here
+  }
+  o0.xyz *= untonemappedSign;
   return;
 }
--- src/games/_madodora/custom_settings.cpp
+++ src/games/_madodora/custom_settings.cpp
@@ -33,14 +33,14 @@ static const std::unordered_map<std::string, float> default_values = {
     {"ColorGradePerChannelBlowoutRestoration", 0.f},
     {"ColorGradePerChannelHueCorrection", 0.f},
     {"ColorGradePerChannelChrominanceCorrection", 0.f},
-    {"ColorGradeClip", 65.f},
+    {"ColorGradeClip", 20.f},
     {"ColorGradeGamma", 50.f},
     {"CustomCharacterBrightness", 1.f},
     {"CustomTextBrightness", 1.f},
     {"CustomBloom2D", 100.f},
     {"CustomBloom3D", 100.f},
     {"EnableUIToneMapPass", 0.f},
-    {"SwapChainCustomColorSpace", 1.f},
+    {"SwapChainCustomColorSpace", 0.f},
     {"IntermediateEncoding", 1.f},
     {"SwapChainDecoding", 1.f},
     {"SwapChainGammaCorrection", 0.f},
@@ -143,7 +143,7 @@ std::vector<renodx::utils::settings::Setting*> GenerateCustomGameSettingsSection
 
 
 const std::unordered_map<std::string, std::pair<reshade::api::format, float>> UPGRADE_TARGETS = {
-    {"R8G8B8A8_TYPELESS", {reshade::api::format::r8g8b8a8_typeless, 3.f}}, // needed for lutbuilder
+    {"R8G8B8A8_TYPELESS", {reshade::api::format::r8g8b8a8_typeless, 2.f}}, // needed for lutbuilder
     {"B8G8R8A8_TYPELESS", {reshade::api::format::b8g8r8a8_typeless, 0.f}},
     {"R8G8B8A8_UNORM", {reshade::api::format::r8g8b8a8_unorm, 0.f}},
     {"B8G8R8A8_UNORM", {reshade::api::format::b8g8r8a8_unorm, 0.f}},
@@ -153,9 +153,11 @@ const std::unordered_map<std::string, std::pair<reshade::api::format, float>> UP
     {"R10G10B10A2_TYPELESS", {reshade::api::format::r10g10b10a2_typeless, 0.f}},
     {"R10G10B10A2_UNORM", {reshade::api::format::r10g10b10a2_unorm, 0.f}},
     {"B10G10R10A2_UNORM", {reshade::api::format::b10g10r10a2_unorm, 0.f}},
-    {"R11G11B10_FLOAT", {reshade::api::format::r11g11b10_float, 3.f}}, // bloom
+    {"R11G11B10_FLOAT", {reshade::api::format::r11g11b10_float, 0.f}}, // bloom
     {"R16G16B16A16_TYPELESS", {reshade::api::format::r16g16b16a16_typeless, 0.f}},
 };
 
 
+inline void AddCustomResourceUpgrades() {
+}
 } // namespace hbr_custom_settings 
