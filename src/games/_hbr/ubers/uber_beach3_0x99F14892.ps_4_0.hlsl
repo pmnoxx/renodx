b29e9528c79a6ac9771c87af4a5f7fffe663c2fe
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-07-25 17:52:50 -0700

    Hotfix for HBR debug

--- src/games/_hbr/ubers/uber_beach3_0x99F14892.ps_4_0.hlsl
+++ src/games/_hbr/ubers/uber_beach3_0x99F14892.ps_4_0.hlsl
@@ -46,7 +46,7 @@ void main(
   r0.xyzw = t0.Sample(s0_s, w1.xy).xyzw;
   r0.w = saturate(r0.w); // fix for bloom
   
-  r0 = debug_mode(r0, v1);
+  r0 = debug_mode(r0, w1);
   // r1 = debug_mode(r1, v1);
 
   r0.xyz = renodx::color::bt709::clamp::BT2020(r0.xyz);
