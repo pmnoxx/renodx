8bf1911be0fffb4393f33c423b5f09848d9da5d8
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-08-15 21:38:06 -0700

    Remove build_ninja from git tracking and update .gitignore
    - Remove build_ninja/ directory from git tracking (build output should not be committed)
    - Add build_ninja/ and build_ninja/** to .gitignore
    - Build directories are generated by CMake and should be ignored

--- build_ninja/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/repos/renodx2
-# Build directory: D:/repos/renodx2/build_ninja
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
