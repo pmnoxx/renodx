a88797d946035a537814de6b43c798da3a0882fe
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-08-21 00:59:19 -0700

    Remove unused files

--- src/addons/display_commander/reshade_events/fullscreen_prevention.cpp
+++ src/addons/display_commander/reshade_events/fullscreen_prevention.cpp
@@ -1,28 +1,11 @@
 #include "fullscreen_prevention.hpp"
 #include "../addon.hpp"
 #include <windows.h>
-#include <sstream>
 
 namespace renodx::display_commander::events {
 
 bool OnSetFullscreenState(reshade::api::swapchain* swapchain, bool fullscreen, void* hmonitor) {
-    // Only prevent fullscreen attempts, not fullscreen->windowed transitions
-    if (!fullscreen) {
-        return false;
-    }
-
-    // Check if fullscreen prevention is enabled in Display Commander
-    if (s_prevent_fullscreen < 0.5f) {
-        return false;
-    }
-
-    // Log the prevention
-    std::ostringstream oss;
-    oss << "[Display Commander] Preventing fullscreen attempt - prevention enabled";
-    LogInfo(oss.str().c_str());
-
-    // Return true to suppress the fullscreen event
-    return true;
+    return fullscreen && s_prevent_fullscreen >= 0.5f;
 }
 
-} // namespace renodx::display_commander::events
+}
--- src/addons/display_commander/reshade_events/fullscreen_prevention.hpp
+++ src/addons/display_commander/reshade_events/fullscreen_prevention.hpp
@@ -4,9 +4,6 @@
 
 namespace renodx::display_commander::events {
 
-// Fullscreen prevention event handler
-// Returns true to suppress the event, false to allow it to proceed
 bool OnSetFullscreenState(reshade::api::swapchain* swapchain, bool fullscreen, void* hmonitor);
 
-} // namespace renodx::display_commander::events
-
+}
