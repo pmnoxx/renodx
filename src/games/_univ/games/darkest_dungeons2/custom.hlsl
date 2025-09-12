c3104dc66a4ee9d0f5a9eecfeee173c68e7fb3ba
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-09-12 03:15:30 -0700

    migrate darkest_dungeon2 to _univ

---
+++ src/games/_univ/games/darkest_dungeons2/custom.hlsl
@@ -0,0 +1,65 @@
+// Custom game color grading/tone mapping helpers
+
+#define CUSTOM_USE_PQ_TONE_MAP_FOR_LUT 1.f
+
+#include "../../common.hlsl"
+
+#ifndef CUSTOM_HLSL_H
+#define CUSTOM_HLSL_H
+
+#define cmp -
+
+float3 UnityLookupARIDPQ(float4 input_color, float2 texcoord, float4 lut_param, SamplerState s0_s, Texture2D<float4> lut) {
+  float4 debug_output = debug_mode(input_color, texcoord, 0.02f);
+  float3 lut_output;
+  if (CUSTOM_USE_PQ_TONE_MAP_FOR_LUT == 1.f) {
+    const float3 bt2020_converted = max(0.f, renodx::color::bt2020::from::BT709(debug_output.xyz));
+    float3 lut_input = renodx::color::pq::Encode(bt2020_converted, 100.f);
+    lut_output = renodx::lut::SampleTetrahedral(lut, saturate(lut_input));
+  } else {
+    // incorrect code
+    float4 r0 = float4(debug_output.xyz, 0.f);
+    float4 r1, r2, r3;
+    r0.xyz = lut_param.www * r0.zxy;
+    r0.xyz = r0.xyz * float3(5.55555582, 5.55555582, 5.55555582) + float3(0.0479959995, 0.0479959995, 0.0479959995);
+    r0.xyz = max(float3(0, 0, 0), r0.xyz);
+    r0.xyz = log2(r0.xyz);
+    r0.xyz = saturate(r0.xyz * float3(0.0734997839, 0.0734997839, 0.0734997839) + float3(0.386036009, 0.386036009, 0.386036009));
+    r0.yzw = lut_param.zzz * r0.xyz;
+
+    r0.y = floor(r0.y);
+    r1.xy = float2(0.5, 0.5) * lut_param.xy;
+    r1.yz = r0.zw * lut_param.xy + r1.xy;
+    r1.x = r0.y * lut_param.y + r1.y;
+
+    // lut query
+    r2.xyzw = lut.SampleLevel(s0_s, r1.xz, 0).xyzw;
+    r3.x = lut_param.y;
+    r3.y = 0;
+    r0.zw = r3.xy + r1.xz;
+    r1.xyzw = lut.SampleLevel(s0_s, r0.zw, 0).xyzw;
+    r0.x = r0.x * lut_param.z + -r0.y;
+    r0.yzw = r1.xyz + -r2.xyz;
+    r0.xyz = r0.xxx * r0.yzw + r2.xyz;
+  }
+  return lut_output;
+}
+
+float3 UnityLookupSDRsRGB(float3 input_color, float2 texcoord, float lut_strength2, Texture2D<float4> lut_2) {
+  float3 untonemappedSign = sign(input_color);
+  float3 untonemappedAbs = abs(input_color);
+  float3 sdrTonemapped = RestoreHighlightSaturation(input_color);
+
+  float3 encoded = renodx::color::srgb::EncodeSafe(sdrTonemapped);
+  float3 lutResult = renodx::lut::SampleTetrahedral(lut_2, encoded);
+  float3 decoded = renodx::color::srgb::DecodeSafe(lutResult);
+
+  float3 graded = ComputeUntonemappedGraded(untonemappedAbs, decoded, sdrTonemapped);
+
+  float3 result = lerp(untonemappedAbs, graded, lut_strength2);
+  result = untonemappedSign * result;
+
+  return result;
+}
+
+#endif
---
+++ src/games/_univ/games/darkest_dungeons2/ubers/lut_0xD8341E94.ps_4_0.hlsl
@@ -0,0 +1,93 @@
+#include "../custom.hlsl"
+
+// ---- Created with 3Dmigoto v1.4.1 on Wed Jul  9 12:54:51 2025
+Texture2D<float4> t1 : register(t1);
+
+Texture2D<float4> t0 : register(t0);
+
+SamplerState s1_s : register(s1);
+
+SamplerState s0_s : register(s0);
+
+cbuffer cb0 : register(b0)
+{
+  float4 cb0[136];
+}
+
+
+
+// 3Dmigoto declarations
+#define cmp -
+
+
+void main(
+  float4 v0 : SV_POSITION0,
+  float2 v1 : TEXCOORD0,
+  out float4 o0 : SV_Target0)
+{
+  float4 r0,r1,r2,r3,r4;
+  uint4 bitmask, uiDest;
+  float4 fDest;
+
+  r0.xyzw = t0.SampleBias(s0_s, v1.xy, cb0[5].x).xyzw;
+  r1.x = cmp(0 < cb0[135].x);
+
+  if (r1.x != 0 && RENODX_TONE_MAP_TYPE != 0.f) {
+    r0.xyz = UnityLookupSDRsRGB(r0.xyz, v1.xy, cb0[135].x, t1);
+    r0.xyz = debug_mode(r0.xyzw, v1.xy, 0.00f).xyz;
+    r0.xyz = renodx::draw::ToneMapPass(r0.xyz);
+    r0.xyz = renodx::draw::RenderIntermediatePass(r0.xyz);
+
+    o0.xyz = r0.xyz;
+    o0.w = 1.f;
+    return;
+  }
+
+
+  if (r1.x != 0) {
+    float3 untonemapped = r0.xyz; // untonemapped here is still in SRGB
+
+    r0.xyz = saturate(untonemapped);
+
+    r1.x = -1 + cb0[133].w;
+    r0.xyz = saturate(r0.xyz);
+    r1.yzw = cmp(float3(0.00313080009,0.00313080009,0.00313080009) >= r0.zxy);
+    r2.xyz = float3(12.9232101,12.9232101,12.9232101) * r0.zxy;
+    r3.xyz = log2(r0.zxy);
+    r3.xyz = float3(0.416666657,0.416666657,0.416666657) * r3.xyz;
+    r3.xyz = exp2(r3.xyz);
+    r3.xyz = r3.xyz * float3(1.05499995,1.05499995,1.05499995) + float3(-0.0549999997,-0.0549999997,-0.0549999997);
+    r1.yzw = r1.yzw ? r2.xyz : r3.xyz;
+
+    r2.xyz = r1.yzw * r1.xxx; // (1- strength)
+    r1.z = floor(r2.x);
+    r2.xw = float2(0.5,0.5) * cb0[133].xy;
+    r2.yz = r2.yz * cb0[133].xy + r2.xw;
+    r2.x = r1.z * cb0[133].y + r2.y;
+    r3.xyzw = t1.SampleLevel(s1_s, r2.xz, 0).xyzw;
+    r4.x = cb0[133].y;
+    r4.y = 0;
+    r2.xy = r4.xy + r2.xz;
+    r2.xyzw = t1.SampleLevel(s1_s, r2.xy, 0).xyzw;
+    r1.x = r1.y * r1.x + -r1.z;
+    r1.yzw = r2.xyz + -r3.xyz;
+    r1.xyz = r1.xxx * r1.yzw + r3.xyz;
+
+    r2.xyz = float3(0.0773993805,0.0773993805,0.0773993805) * r1.xyz;
+    r3.xyz = float3(0.0549999997,0.0549999997,0.0549999997) + r1.xyz;
+    r3.xyz = float3(0.947867334,0.947867334,0.947867334) * r3.xyz;
+    r3.xyz = log2(abs(r3.xyz));
+    r3.xyz = float3(2.4000001,2.4000001,2.4000001) * r3.xyz;
+    r3.xyz = exp2(r3.xyz);
+    r1.xyz = cmp(float3(0.0404499993,0.0404499993,0.0404499993) >= r1.xyz);
+    r1.xyz = r1.xyz ? r2.xyz : r3.xyz;
+
+    r1.xyz = r1.xyz + -r0.xyz;
+    r0.xyz = cb0[135].xxx * r1.xyz + r0.xyz;
+  }
+
+
+  o0.xyzw = r0.xyzw;
+
+  return;
+}
